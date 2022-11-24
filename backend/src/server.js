60e2a454989b3603a7d887de658b2c9852d93971
Alexander Sidorenko <asiddev@protonmail.com>
2022-11-24 15:29:55 -0800

    add default port

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ const app = Express();
 
 app.use("/api/users", userRoutes);
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 8800, () => {
   console.log(
     "Succefully Starting Server : listening on port " + process.env.PORT
   );
