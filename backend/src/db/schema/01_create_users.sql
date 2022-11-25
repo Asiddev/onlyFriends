c535bddf5f21388d8562696b3178d9f2419d166a
Kevin Lee <jhssttj@hotmail.com>
2022-11-25 15:04:07 -0700

    Update user schema, add match schema

--- backend/src/db/schema/01_create_users.sql
+++ backend/src/db/schema/01_create_users.sql
@@ -2,6 +2,12 @@ DROP TABLE IF EXISTS users CASCADE;
 -- CREATE USERS
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
-  password VARCHAR(255)
+  password VARCHAR(255) NOT NULL,
+  profile_picture VARCHAR(255),
+  banner_picture VARCHAR(255),
+  description TEXT(100),
+  location VARCHAR(255),
+  created_at DATE NOT NULL
 );
--- backend/src/db/seeds/01_users.sql
+++ backend/src/db/seeds/01_users.sql
@@ -2,3 +2,7 @@ INSERT INTO users (id, email, password) VALUES (1, 'rick.sandchez@gmail.com', 'p
 INSERT INTO users (id, email, password) VALUES (2, 'lisa.simpson@gmail.com', 'ehhhhh');
 INSERT INTO users (id, email, password) VALUES (3, 'link@yahoo.com', 'hyrule');
 INSERT INTO users (id, email, password) VALUES (4, 'simon_bel123@mail.ca', 'dracula');
+
+
+
+INSERT INTO users (id, name, email, password, profile_picture, banner_picture, description, location, created_date)
