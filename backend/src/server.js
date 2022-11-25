a503dfd8f4437ae2cfe99db83cf080d2ce5aa17d
Alexander Sidorenko <asiddev@protonmail.com>
2022-11-24 17:00:05 -0800

    server middlewares

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,25 @@
 require("dotenv").config();
-const Express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const express = require("express");
 const userRoutes = require("./routes/userRoutes");
 
-const app = Express();
+const app = express();
 
+//middlewares
+app.use(cookieParser());
+app.use(cors());
+app.use(express.json());
+//
+
+//routes
 app.use("/api/users", userRoutes);
+//
 
+//server connection
 app.listen(process.env.PORT || 8800, () => {
   console.log(
     "Succefully Starting Server : listening on port " + process.env.PORT
   );
 });
+//
