176f1ae6eebef4d67d1ad74c79bf04dac8937b4b
Alexander Sidorenko <asiddev@protonmail.com>
2022-11-25 12:27:03 -0800

    scss plan

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,6 +2,10 @@ import "./App.scss";
 import LoginPage from "./landingPage/login/Login";
 import Register from "./landingPage/register/Register";
 
-function App() {}
+function App() {
+  let user = true;
+
+  return <div>{user ? <LoginPage /> : <Register />}</div>;
+}
 
 export default App;
