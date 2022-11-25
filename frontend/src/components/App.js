7324e5b4799c84d67a34423477ecbce8005ff53f
nicohsfu <marcnicohernandez@gmail.com>
2022-11-25 12:27:35 -0800

    material UI - buttons and chips demo

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,14 +1,26 @@
 import * as React from 'react';
-import { Button } from '@mui/material';
+import { Button, Chip } from '@mui/material';
 
 function App() {
   return (
     <div>
-      <h1>Different button variants</h1>
+      <h1>Material UI - demos</h1>
+
+      <h2>buttons</h2>
       <Button variant="contained">contained</Button>
       <Button variant="outlined">outlined</Button>
       <Button variant="text">text</Button>
       <Button variant="string">string</Button>
+
+      <h2>chips</h2>
+      <Chip label="Chip variant Outlined - color primary" variant="outlined" color="primary"/>
+      <Chip label="Chip variant Filled - color primary" variant="filled" color="primary" />
+      <Chip label="Chip variant Outlined - color primary" variant="outlined" color="primary"/>
+      <Chip label="Chip variant Outlined - color primary" variant="outlined" color="primary"/>
+      <Chip label="Chip variant Outlined - color primary" variant="outlined" color="primary"/>
+      <Chip label="Chip variant Filled - color primary" variant="filled" color="primary" />
+      {/* <Chip label="Clickable" onClick={handleClick} />
+      <Chip label="Clickable" variant="outlined" onClick={handleClick} /> */}
     </div>
   );
 }
