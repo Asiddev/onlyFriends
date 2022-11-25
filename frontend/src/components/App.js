0b004e8bd40907b9d72dcc87f18a2eface331850
Alexander Sidorenko <asiddev@protonmail.com>
2022-11-25 12:48:33 -0800

    change root for router

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -9,7 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </>
