f3f9fe39d9edb38314f42217093fee19ac993ab7
Alexander Sidorenko <asiddev@protonmail.com>
2022-11-25 18:09:32 -0800

    axios sends information and saved by database

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,12 +1,23 @@
-const userQueries = require('../db/queries/users');
+const userQueries = require("../db/queries/users");
 
 const getAllUsers = (req, res) => {
-  userQueries.getAllUsers()
-  .then(users => {
+  userQueries.getAllUsers().then((users) => {
     res.send(users);
-  })
+  });
+};
+
+const addUser = (req, res) => {
+  //set cookie
+  //encryption
+  //hash password
+
+  userQueries.addUser(req.body.email, req.body.password).then((data) => {
+    console.log("added user!");
+    console.log(data.rows[0]);
+  });
 };
 
 module.exports = {
   getAllUsers,
+  addUser,
 };
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -4,5 +4,6 @@ const userController = require("../controllers/userController.js");
 const router = express.Router();
 
 router.get("/", userController.getAllUsers);
+router.post("/", userController.addUser);
 
 module.exports = router;
