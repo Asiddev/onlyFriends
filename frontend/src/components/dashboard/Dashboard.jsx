bcfa023b60343e8fcb88437ade4b6ffbe0543192
nicohsfu <marcnicohernandez@gmail.com>
2022-11-27 00:31:03 -0800

    front-end implementation for the profile setup page

--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -1,104 +1,243 @@
 import React from "react";
-import { AppBar, Box, Toolbar, Typography, Button, IconButton, Avatar, TextField, Chip } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
+import { AppBar, Box, Toolbar, Typography, Button, IconButton, Avatar, TextField, Chip, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Stack, Container, Link, createTheme, ThemeProvider, } from '@mui/material';
 import "./Dashboard.scss";
+
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © Nico Hernandez, Alex Sidor, Kevin Lee "}
+      <Link color="inherit" href="https://github.com/Asiddev/onlyFriends">
+        OnlyFriends
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const theme = createTheme();
 
 function Dashboard(props) {
 
   return (
     <div>
 
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar className="navbar-bg-color">
-            <img
+      <Box marginBottom={10}>
+        <AppBar>
+          <Toolbar className="navbar-logo">
+            {/* <img
               src="https://i.imgur.com/Bgur1Fk.png"
               alt="OnlyFriends logo"
-              style={{ width: "10rem", paddingBottom: "1rem" }}
+              style={{ width: "10rem", alignItems: "center", justifyContent: "center" }}
+            /> */}
+            <Box
+              component="img"
+              sx={{ width: 150 }}
+              alt="OnlyFriends logo"
+              src="https://i.imgur.com/Bgur1Fk.png"
             />
           </Toolbar>
         </AppBar>
       </Box>
 
-      <br />
-
-      <Box>
-        <Typography variant="h5">
+      <Container maxWidth="sm">
+        <Typography
+          variant="h4"
+          align="center"
+          color="text.primary"
+          gutterBottom
+        >
           Profile Setup
         </Typography>
-        <Typography variant="p">
+        <Typography variant="p" align="center" color="text.secondary" paragraph>
           Provide the necessary information to start finding like-minded people!
         </Typography>
-      </Box>
+      </Container>
 
       <br />
 
-      <Box>
+      <Container maxWidth="sm">
         <Typography variant="p">
           Upload a profile picture
         </Typography>
-        <Avatar>
-          test
-        </Avatar>
-      </Box>
+        <Button
+          variant="contained"
+          component="label"
+        >
+          Upload File
+          <input
+            type="file"
+            hidden
+          />
+        </Button>
+      </Container>
 
       <br />
 
-      <Box>
+      <Container maxWidth="sm">
         <Typography variant="p">
           Location
         </Typography>
-        <TextField label="e.g. Vancouver"></TextField>
-      </Box>
+        <TextField label="Location" placeholder="e.g. Vancouver"></TextField>
+      </Container>
 
       <br />
 
-      <Box>
+      <Container maxWidth="sm">
         <Typography variant="p">
           Bio
         </Typography>
-        <TextField label="e.g. I love long walks to the fridge"></TextField>
-      </Box>
+        <TextField label="Bio" placeholder="e.g. I love long walks to the fridge"></TextField>
+      </Container>
 
       <br />
 
-      <Box>
+      <Container maxWidth="sm">
         <Typography variant="p">
           Upload a cover banner
         </Typography>
-        <Avatar>
-          test
-        </Avatar>
-      </Box>
+        <Button
+          variant="contained"
+          component="label"
+        >
+          Upload File
+          <input
+            type="file"
+            hidden
+          />
+        </Button>
+      </Container>
 
       <br />
 
-      <Box>
+      <Container maxWidth="sm" className="chip-spacing">
         <Typography variant="p">
           Select all interests/hobbies that apply
         </Typography>
 
-        <br /><br />
+        <br />
 
         <Chip label="Archery" variant="outlined" color="primary" />
         <Chip label="Axe-throwing" variant="filled" color="primary" />
-        <Chip label="Climbing" variant="outlined" color="primary" />
+        <Chip label="Climbing" variant="filled" color="primary" />
         <Chip label="Fencing" variant="filled" color="primary" />
         <Chip label="Kayaking" variant="outlined" color="primary" />
+        <Chip label="Polevaulting" variant="outlined" color="primary" />
+        <Chip label="Archery" variant="filled" color="primary" />
+        <Chip label="Axe-throwing" variant="outlined" color="primary" />
+        <Chip label="Climbing" variant="outlined" color="primary" />
+        <Chip label="Fencing" variant="filled" color="primary" />
+        <Chip label="Kayaking" variant="filled" color="primary" />
         <Chip label="Polevaulting" variant="outlined" color="primary" />
         <Chip label="Archery" variant="outlined" color="primary" />
         <Chip label="Axe-throwing" variant="filled" color="primary" />
-        <Chip label="Climbing" variant="outlined" color="primary" />
+        <Chip label="Climbing" variant="filled" color="primary" />
         <Chip label="Fencing" variant="filled" color="primary" />
         <Chip label="Kayaking" variant="outlined" color="primary" />
         <Chip label="Polevaulting" variant="outlined" color="primary" />
-      </Box>
+        <Chip label="Archery" variant="filled" color="primary" />
+        <Chip label="Axe-throwing" variant="outlined" color="primary" />
+        <Chip label="Climbing" variant="outlined" color="primary" />
+        <Chip label="Fencing" variant="filled" color="primary" />
+        <Chip label="Kayaking" variant="filled" color="primary" />
+        <Chip label="Polevaulting" variant="outlined" color="primary" />
+        <Chip label="Archery" variant="outlined" color="primary" />
+        <Chip label="Axe-throwing" variant="filled" color="primary" />
+        <Chip label="Climbing" variant="filled" color="primary" />
+        <Chip label="Fencing" variant="filled" color="primary" />
+        <Chip label="Kayaking" variant="outlined" color="primary" />
+        <Chip label="Polevaulting" variant="outlined" color="primary" />
+        <Chip label="Archery" variant="filled" color="primary" />
+        <Chip label="Axe-throwing" variant="outlined" color="primary" />
+        <Chip label="Climbing" variant="outlined" color="primary" />
+        <Chip label="Fencing" variant="filled" color="primary" />
+        <Chip label="Kayaking" variant="filled" color="primary" />
+        <Chip label="Polevaulting" variant="outlined" color="primary" />
+      </Container>
 
       <br />
 
-      <Button variant="contained">
-        Save
-      </Button>
+      <Container maxWidth="sm">
+        <Button variant="contained">
+          Save
+        </Button>
+      </Container>
+
+      <br /><br />
+
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <main>
+          {/* Hero unit */}
+          <Box
+            sx={{
+              bgcolor: 'background.paper',
+              pt: 8,
+              pb: 6,
+            }}
+          >
+
+          </Box>
+          <Container sx={{ py: 8 }} maxWidth="md">
+            {/* End hero unit */}
+            <Grid container spacing={4}>
+              {cards.map((card) => (
+                <Grid item key={card} xs={12} sm={6} md={4}>
+                  <Card
+                    sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                  >
+                    <CardMedia
+                      component="img"
+                      sx={{
+                        // 16:9
+                        pt: '56.25%',
+                      }}
+                      image="https://cdn.iconscout.com/icon/free/png-256/imgur-1-461805.png"
+                      alt="random"
+                    />
+                    <CardContent sx={{ flexGrow: 1 }}>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        Heading
+                      </Typography>
+                      <Typography>
+                        This is a media card. You can use this section to describe the
+                        content.
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small">View</Button>
+                      <Button size="small">Edit</Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+          </Container>
+        </main>
+        {/* Footer */}
+        <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+          <Typography variant="h6" align="center" gutterBottom>
+            Footer
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            align="center"
+            color="text.secondary"
+            component="p"
+          >
+            Something here to give the footer a purpose!
+          </Typography>
+          <Copyright />
+        </Box>
+        {/* End footer */}
+      </ThemeProvider>
 
     </div>
   );
