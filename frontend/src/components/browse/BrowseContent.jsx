1b55f1448762e5614fa6c4393c35d9a5cda98859
nicohsfu <marcnicohernandez@gmail.com>
2022-12-01 19:38:59 -0800

    browse page overhauled - 2 buttons left to position

--- frontend/src/components/browse/BrowseContent.jsx
+++ frontend/src/components/browse/BrowseContent.jsx
@@ -61,87 +61,145 @@ function BrowseContent(props) {
 
   return (
     <>
-      <Box
-        sx={{
-          borderRadius: "1.75rem",
-          backgroundColor: "#E4F8FF",
-          // border: "3px dashed blue",
-          // padding: "2rem 5rem 2rem 5rem",
-          padding: "2rem",
-          display: 'flex column'
-        }}
-      >
+      {/* <Box sx={{ display: 'flex' }}> */}
 
-        {/* FIX THESE BUTTONS */}
-        <Button class="noselect" id="button-left" onClick={swipeReject}>
-          <CloseIcon fontSize="large" />
-        </Button>
-        <Button class="noselect" id="button-right" onClick={swipeAccept}>
-          <CheckIcon fontSize="large" />
-        </Button>
+        {/* <Box
+          sx={{
+            // border: '3px solid red',
+            display: "flex",
+            alignItems: 'center'
+          }}
+        >
+          <Button class="noselect button-left-color" onClick={swipeReject}>
+            <CloseIcon fontSize="large" />
+          </Button>
+        </Box> */}
 
         <Box
           sx={{
-            display: 'flex',
-            alignItems: 'center',
-            marginBottom: '1rem',
+            borderRadius: "1.75rem",
+            backgroundColor: "#E4F8FF",
+            // border: "3px dashed blue",
+            // padding: "2rem 5rem 2rem 5rem",
+            padding: "2rem",
+            display: 'flex column'
           }}
         >
+
+          <Button class="noselect" id="button-left" onClick={swipeReject}>
+            <CloseIcon fontSize="large" />
+          </Button>
+          <Button class="noselect" id="button-right" onClick={swipeAccept}>
+            <CheckIcon fontSize="large" />
+          </Button>
+
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              marginBottom: '1rem',
+            }}
+          >
+            <Box
+              component="img"
+              sx={{ width: 55, height: 55, borderRadius: "100%" }}
+              alt="Current potential match profile pic"
+              src={similarUsers.length ? similarUsers[page].profile_picture : ""}
+            />
+
+            <Box sx={{ marginLeft: '1rem' }}>
+              <Typography variant="h5">
+                {similarUsers.length ? similarUsers[page].name : ""}
+              </Typography>
+            </Box>
+          </Box>
+
           <Box
             component="img"
-            sx={{ width: 55, height: 55, borderRadius: "100%" }}
-            alt="Current potential match profile pic"
-            src={similarUsers.length ? similarUsers[page].profile_picture : ""}
+            sx={{ width: "100%", height: "20rem", objectFit: 'cover', marginBottom: '1rem' }}
+            alt="Current potential match banner"
+            src={similarUsers.length ? similarUsers[page].banner_picture : ""}
           />
 
-          <Box sx={{ marginLeft: '1rem' }}>
-            <Typography variant="h5">
-              {similarUsers.length ? similarUsers[page].name : ""}
+          <Box
+            sx={{ marginBottom: '1rem' }}
+          >
+            <Typography variant="p">
+              <RoomIcon fontSize="sm" />
+              {similarUsers.length ? similarUsers[page].location : ""}
             </Typography>
           </Box>
-        </Box>
 
-        <Box
-          component="img"
-          sx={{ width: "100%", height: "20rem", objectFit: 'cover', marginBottom: '1rem' }}
-          alt="Current potential match banner"
-          src={similarUsers.length ? similarUsers[page].banner_picture : ""}
-        />
-
-        <Box
-          sx={{ marginBottom: '1rem' }}
-        >
-          <Typography variant="p">
-            <RoomIcon fontSize="sm" />
-            {similarUsers.length ? similarUsers[page].location : ""}
-          </Typography>
-        </Box>
-
-        <Box
-          sx={{ marginBottom: '1rem' }}
-        >
-          <Typography
-            variant="body2"
-            color="text.secondary"
+          <Box
+            sx={{ marginBottom: '1rem' }}
           >
-            {similarUsers.length ? similarUsers[page].description : ""}
-          </Typography>
+            <Typography
+              variant="body2"
+              color="text.secondary"
+            >
+              {similarUsers.length ? similarUsers[page].description : ""}
+            </Typography>
+          </Box>
+
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center'
+            }}
+          >
+            <Typography variant="h6">Interests</Typography>
+          </Box>
+
+          <Grid container className="interests-container">
+            <Grid item>{profileInterests && renderInterestList}</Grid>
+          </Grid>
+
+          <Box sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            marginTop: '2rem',
+            gap: '3rem',
+            border: '3px dashed red',
+          }}>
+            <Box
+              sx={{
+                // border: '3px solid red',
+                alignItems: 'center'
+              }}
+            >
+              <Button class="noselect button-left-color" onClick={swipeReject}>
+                <CloseIcon fontSize="large" />
+              </Button>
+            </Box>
+
+            <Box
+              sx={{
+                // border: '3px solid red',
+                alignItems: 'center'
+              }}
+            >
+              <Button class="noselect button-right-color" onClick={swipeAccept}>
+                <CheckIcon fontSize="large" />
+              </Button>
+            </Box>
+          </Box>
+
         </Box>
 
-        <Box
+        {/* <Box
           sx={{
-            display: 'flex',
-            justifyContent: 'center'
+            // border: '3px solid red',
+            display: "flex",
+            alignItems: 'center'
           }}
         >
-          <Typography variant="h6">Interests</Typography>
-        </Box>
+          <Button class="noselect button-right-color" onClick={swipeAccept}>
+            <CheckIcon fontSize="large" />
+          </Button>
+        </Box> */}
 
-        <Grid container className="interests-container">
-          <Grid item>{profileInterests && renderInterestList}</Grid>
-        </Grid>
-
-      </Box>
+      {/* </Box> */}
     </>
   );
 }
