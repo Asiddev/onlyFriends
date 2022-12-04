0bad14a84c672bb7e64cba38d7bfd3a644d37ee2
nicohsfu <marcnicohernandez@gmail.com>
2022-12-04 11:44:56 -0800

    change class attributes to className attributes

--- frontend/src/components/browse/EndAnimation.jsx
+++ frontend/src/components/browse/EndAnimation.jsx
@@ -4,16 +4,16 @@ import Copyright from "../Copyright";
 function EndAnimation() {
   return (
     <>
-      <div class="page_404 body">
-        <div class="row">
-          <div class="col-sm-12 ">
-            <div class="contant_box_404">
-              <h3 class="h2">No more users in your area</h3>
+      <div className="page_404 body">
+        <div className="row">
+          <div className="col-sm-12 ">
+            <div className="contant_box_404">
+              <h3 className="h2">No more users in your area</h3>
 
               <p>Please check back after some time</p>
             </div>
-            <div class="col-sm-10 col-sm-offset-1  text-center">
-              <div class="four_zero_four_bg"></div>
+            <div className="col-sm-10 col-sm-offset-1  text-center">
+              <div className="four_zero_four_bg"></div>
             </div>
           </div>
         </div>
