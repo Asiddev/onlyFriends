1af767746b0378660f2749848849e0a96b267cb5
Kevin Lee <jhssttj@hotmail.com>
2022-12-05 13:34:51 -0700

    alter seed data for user and userinterest

--- backend/src/db/seeds/01_users.sql
+++ backend/src/db/seeds/01_users.sql
@@ -1,13 +1,13 @@
 INSERT INTO users (name, email, password, profile_picture, banner_picture, description, location, created_at) 
 VALUES 
-('Aldwin Bautista', 'Aldiwn@email.com', '$2b$10$B0Yjjbke2i0cRJ.31OtBYOxuYVB9BcEGwopaVlOU9svnVVd2tXrXG','https://i.imgur.com/QMntCB7.png','https://i.imgur.com/qdVnqRJ.jpg','I love being in deep thoughts and providing for the ones I care', 'Vancouver, BC, Canada', '2022-11-25'),
-('Jakob Bollman', 'Jakob@email.com', '$2b$10$B0Yjjbke2i0cRJ.31OtBYOxuYVB9BcEGwopaVlOU9svnVVd2tXrXG','https://i.imgur.com/g7PB1Nk.jpg','https://i.imgur.com/UigZzwS.jpg','I love skiing and people with amazing smile like me. Hit me up if you like to smile', 'Vancouver, BC, Canada', '2022-11-25'),
-('Jaxson Berg', 'Jaxson@email.com', '$2b$10$B0Yjjbke2i0cRJ.31OtBYOxuYVB9BcEGwopaVlOU9svnVVd2tXrXG','https://i.imgur.com/lgDJDBu.png','https://i.imgur.com/LVQui1O.png','I love coding and playing story driven games. I also have majestic hair', 'Calgary, AB, Canada', '2022-11-25'),
-('Kasey Valdez', 'Kasey@email.com', '$2b$10$B0Yjjbke2i0cRJ.31OtBYOxuYVB9BcEGwopaVlOU9svnVVd2tXrXG','https://i.imgur.com/QaxJGky.jpg','https://i.imgur.com/CIIjCnO.jpg','I love using profile pic not of myself and big chungus', 'Vancouver, BC, Canada', '2022-11-25'),
-('Emily Vilett', 'Emily@email.com', '$2b$10$B0Yjjbke2i0cRJ.31OtBYOxuYVB9BcEGwopaVlOU9svnVVd2tXrXG','https://i.imgur.com/fxBIvnA.jpg','https://i.imgur.com/f1WF67l.jpg','I am in dire need of friends for my only friends are a plushy parrot and a turnip', 'Vancouver, BC, Canada', '2022-11-25'),
+('Aldwin Bautista', 'Aldiwn@email.com', '$2b$10$B0Yjjbke2i0cRJ.31OtBYOxuYVB9BcEGwopaVlOU9svnVVd2tXrXG','https://i.imgur.com/QMntCB7.png','https://i.imgur.com/qdVnqRJ.jpg','I have this great idea for a tamogatchi web game...hit me up to learn more', 'Vancouver, BC, Canada', '2022-11-25'),
+('Jakob Bollman', 'Jakob@email.com', '$2b$10$B0Yjjbke2i0cRJ.31OtBYOxuYVB9BcEGwopaVlOU9svnVVd2tXrXG','https://i.imgur.com/g7PB1Nk.jpg','https://i.imgur.com/UigZzwS.jpg','I love skiing and people with amazing smile like me. Hit me up if you have a radiant smile', 'Vancouver, BC, Canada', '2022-11-25'),
+('Jaxson Berg', 'Jaxson@email.com', '$2b$10$B0Yjjbke2i0cRJ.31OtBYOxuYVB9BcEGwopaVlOU9svnVVd2tXrXG','https://i.imgur.com/lgDJDBu.png','https://i.imgur.com/LVQui1O.png','I love coding and secretly a spy in a discord group. I also have majestic hair silky af. ', 'Vancouver, BC, Canada', '2022-11-25'),
+('Kasey Valdez', 'Kasey@email.com', '$2b$10$B0Yjjbke2i0cRJ.31OtBYOxuYVB9BcEGwopaVlOU9svnVVd2tXrXG','https://i.imgur.com/QaxJGky.jpg','https://i.imgur.com/CIIjCnO.jpg','I love using profile pic not of myself and big chungus. Hit me up if you are into making meme gifs', 'Vancouver, BC, Canada', '2022-11-25'),
+('Emily Vilett', 'Emily@email.com', '$2b$10$B0Yjjbke2i0cRJ.31OtBYOxuYVB9BcEGwopaVlOU9svnVVd2tXrXG','https://i.imgur.com/fxBIvnA.jpg','https://i.imgur.com/f1WF67l.jpg','Mercy main in dire need of friends to play Overwatch2. As you can see, my only friends are plushies.', 'Vancouver, BC, Canada', '2022-11-25'),
 ('Taylor Smith', 'Taylor@email.com', '$2b$10$B0Yjjbke2i0cRJ.31OtBYOxuYVB9BcEGwopaVlOU9svnVVd2tXrXG','https://i.imgur.com/NVyfREI.jpg','https://i.imgur.com/gHiuXxU.png','I love over-dressing for casual situations Please do not talk to me if you dont own a $1000+ suit.', 'Vancouver, BC, Canada', '2022-11-25'),
 ('Ray Yu', 'Ray@email.com', '$2b$10$B0Yjjbke2i0cRJ.31OtBYOxuYVB9BcEGwopaVlOU9svnVVd2tXrXG','https://i.imgur.com/JNxypzl.png','https://i.imgur.com/RWoCBv3.png','I run a discord server w/over 5000+ members Please dm me if you want to be part of the best cohort.', 'Vancouver, BC, Canada', '2022-11-25'),
-('Minha Kim', 'Minha@email.com', '$2b$10$B0Yjjbke2i0cRJ.31OtBYOxuYVB9BcEGwopaVlOU9svnVVd2tXrXG','https://i.imgur.com/laelNmQ.jpeg','https://i.imgur.com/gc7foG7.jpg','Shawl enthusiast and a strawberry farmer. Love meeting with people named Anouk and kasey ', 'Vancouver, BC, Canada', '2022-11-25'), 
+('Minha Kim', 'Minha@email.com', '$2b$10$B0Yjjbke2i0cRJ.31OtBYOxuYVB9BcEGwopaVlOU9svnVVd2tXrXG','https://i.imgur.com/laelNmQ.jpeg','https://i.imgur.com/gc7foG7.jpg','Shawl enthusiast and a strawberry consumer. Love meeting with people named Anouk and Kasey ', 'Vancouver, BC, Canada', '2022-11-25'), 
 
 -- Person #9 onwards, template below:
 -- ('Name', 'name@email.com', '$2b$10$B0Yjjbke2i0cRJ.31OtBYOxuYVB9BcEGwopaVlOU9svnVVd2tXrXG','profPic','bannerPic','bioHere', 'Vancouver, BC, Canada', '2022-12-04'),
@@ -15,8 +15,9 @@ VALUES
 ('Coach Sanders', 'coach@email.com', '$2b$10$B0Yjjbke2i0cRJ.31OtBYOxuYVB9BcEGwopaVlOU9svnVVd2tXrXG','https://i.imgur.com/FZ0U6gr.png','https://i.imgur.com/bXbapKJ.png','Elementary football coach, never lost. I sub in when I have to. Whatever it takes to win.', 'Vancouver, BC, Canada', '2022-12-04'),
 ('John Gray', 'john@email.com', '$2b$10$B0Yjjbke2i0cRJ.31OtBYOxuYVB9BcEGwopaVlOU9svnVVd2tXrXG','https://i.imgur.com/YstUtc9.png','https://i.imgur.com/YWIsjZX.png','Some guy', 'Vancouver, BC, Canada', '2022-12-04'),
 ('Supahot Fire', 'supahot@email.com', '$2b$10$B0Yjjbke2i0cRJ.31OtBYOxuYVB9BcEGwopaVlOU9svnVVd2tXrXG','https://i.imgur.com/QgsjZgB.png','https://i.imgur.com/eQ4XHYc.png','I spit hot fire', 'Vancouver, BC, Canada', '2022-12-04'),
-('Speedy Hernandez', 'speedy@email.com', '$2b$10$B0Yjjbke2i0cRJ.31OtBYOxuYVB9BcEGwopaVlOU9svnVVd2tXrXG','https://i.imgur.com/33C92cP.png','https://i.imgur.com/Ry6gVFw.jpg','#95. Racecar driver. Life is a highway.', 'Vancouver, BC, Canada', '2022-12-04')
-;
+('Speedy Hernandez', 'speedy@email.com', '$2b$10$B0Yjjbke2i0cRJ.31OtBYOxuYVB9BcEGwopaVlOU9svnVVd2tXrXG','https://i.imgur.com/33C92cP.png','https://i.imgur.com/Ry6gVFw.jpg','#95. Racecar driver. Life is a highway.', 'Vancouver, BC, Canada', '2022-12-04'),
+
 
 ('Mr.Roll', 'RR@email.com', '$2b$10$B0Yjjbke2i0cRJ.31OtBYOxuYVB9BcEGwopaVlOU9svnVVd2tXrXG','https://imgur.com/a/lhorJ5n','https://imgur.com/a/gGDlLhF','Check out my new hit single!', 'Vancouver, BC, Canada', '2022-12-04'),
-('Hordor', 'Hordor@homedepot.com', '$2b$10$B0Yjjbke2i0cRJ.31OtBYOxuYVB9BcEGwopaVlOU9svnVVd2tXrXG','https://imgur.com/a/219RnvK','https://imgur.com/a/hS0vNkN','Doors and door accessories', 'Vancouver, BC, Canada', '2022-12-04'),
+('Hordor', 'Hordor@homedepot.com', '$2b$10$B0Yjjbke2i0cRJ.31OtBYOxuYVB9BcEGwopaVlOU9svnVVd2tXrXG','https://imgur.com/a/219RnvK','https://imgur.com/a/hS0vNkN','Doors and door accessories', 'Vancouver, BC, Canada', '2022-12-04')
+;
